18a56e9f95222299591998803cbc600e6e299854
Preston Bennes <preston.bennes@gmail.com>
2015-02-22 16:13:59 -0700

    algo1/w4/test_graph_primitivies.py: change dfs tests to numeric vertex identifiers, expand pre/post/reverse-post ordering

--- coursera/algo-pt1/week4/test_graph_primitivies.py
+++ coursera/algo-pt1/week4/test_graph_primitivies.py
@@ -7,8 +7,9 @@ from graph_primitives import get_graph_finishing_times
 
 def test_topological_order():
     """
-    Input: A directed graph represented as a list of tuples. Each tuple
-           represents verticies at the tail and head of an arc
+    Input: topological_order(graph)
+        graph: A directed graph represented as a list of tuples. Each tuple
+               represents verticies at the tail and head of an arc
 
         ex. graph = [(1, 2), (1, 3), (2, 4), (3, 4)]
 
@@ -49,31 +50,55 @@ def test_topological_order():
 
 def test_dfs():
     """
-    Given a directed graph represented as a dict of lists, where each key
-    represents the tail of an arc and each list entry represents the head of an
-    arc, return the traversed in-order path of each position in the graph
+    Input: dfs(graph, node)
+        graph: A directed graph represented as a list of tuples. Each tuple
+               represents verticies at the tail and head of an arc
+        node:  A starting vertex, represented as an integer
+
+        ex.
+            graph = [(1, 2), (1, 3), (2, 4), (3, 4)]
+            node  = 2
+
+        Graph visual representation
+
+              2
+              o
+            1↗ ↘4
+            o   o
+             ↘ ↗
+              o
+              3
+
+    Output:
+        So, there are a couple of possible outputs.
+
+        graph = [(1, 2), (1, 3), (2, 4), (3, 4)]
+
+        preordering
+            A list of verticies in the order they were virst visited
+            dfs(graph, 1)
+            # Depending on whether 2 or 3 is chosen first
+            > (1, 2, 4, 2, 1, 3, 1) or (1, 3, 4, 3, 1, 2, 1)
+            # If backtracking is not tracked
+            > (1, 2, 4, 3) or (1, 3, 4, 2)
+
+        postordering
+            A list of verticies in the order they were *last* visited
+            dfs(graph, 1)
+            > (4, 3, 2, 1) or (4, 2, 3, 1)
+
+        reverse postordering
+            A reverse of postordering
+            dfs(graph, 1)
+            > (1, 2, 3, 4) or (1, 3, 2, 4)
     """
-    graph = {
-        's': ['v', 'w'],
-        'v': ['t'],
-        'w': ['t'],
-        't': [],
-    }
-    """
-    Graph visual representation
-          v
-          o
-        s↗ ↘t
-        o   o
-         ↘ ↗
-          o
-          w
-    """
-    assert dfs(graph, 't') == ['t']
-    assert dfs(graph, 'v') == ['v', 't']
-    assert dfs(graph, 'w') == ['w', 't']
-    assert dfs(graph, 's') in [['s', 'v', 't', 'w'],
-                               ['s', 'w', 't', 'v']]
+    graph = [(1, 2), (1, 3), (2, 4), (3, 4)]
+    # preordering with backtracking
+    assert dfs(graph, 1) in [(1, 2, 4, 2, 1, 3, 1), (1, 3, 4, 3, 1, 2, 1)]
+    assert dfs(graph, 2) == (2, 4)
+    assert dfs(graph, 3) == (3, 4)
+    assert dfs(graph, 4) == (4)
+
 def test_get_strongly_connected_components():
     """
     Given a directed graph represented as a dict of lists, where each key
