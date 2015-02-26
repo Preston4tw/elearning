227a244685ba5e6ef105172d9c09ea100d6ac9a9
Preston Bennes <preston.bennes@gmail.com>
2015-02-25 23:52:15 -0700

    algo1/w5/dijkstra.py: initial commit

---
+++ coursera/algo-pt1/week5/dijkstra.py
@@ -0,0 +1,9 @@
+from collections import namedtuple
+
+def compute_shortest_paths(graph, starting_vertex):
+    """
+    Given a graph (adj list) and starting vertex, compute path lengths to all
+    other verticies and return a hash where each hash key is the ending node and
+    the hash value is the distance of the shortest path
+    """
+    pass
