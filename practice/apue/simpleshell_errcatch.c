38097c94a84afcaf12804dd9767542274cd6f3ed
Preston Bennes <preston.bennes@gmail.com>
2015-05-02 11:26:21 -0700

    apue/simpleshell_errcatch: demo signal handlers

---
+++ practice/apue/simpleshell_errcatch.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+#define MAXLINE 4096
+
+static void sig_int(int);
+
+int main(void) {
+  char buf[MAXLINE];
+  pid_t pid;
+  int status;
+
+  if (signal(SIGINT, sig_int) == SIG_ERR) {
+    printf("signal error\n");
+    exit(1);
+  }
+
+  printf("%% ");
+  while (fgets(buf, MAXLINE, stdin) != NULL) {
+    if (buf[strlen(buf) - 1] == '\n')
+      buf[strlen(buf) - 1] = 0;
+
+    if ((pid = fork()) < 0) {
+      printf("fork error\n");
+      exit(1);
+    } else if (pid == 0) { // child
+      execlp(buf, buf, (char *)0);
+      printf("couldn't execute: %s", buf);
+      exit(127);
+    }
+    // parent
+    if ((pid = waitpid(pid, &status, 0)) < 0) {
+      printf("waitpid error");
+    }
+    printf("%% ");
+  }
+  exit(0);
+}
+
+void sig_int(int signo) {
+  printf("interrupt\n%% ");
+}
